f17dd25c1d5c38cfb3f1271ad8664fdac68b2350
muhammadsadri19 <muhammadsadry19@gmail.com>
2024-08-26 23:40:43 +0700

    Configuration : Connect to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:muuri_app/config/firebase_options.dart';
 import 'package:muuri_app/pages/search_page.dart';
+import 'package:muuri_app/utils/constant.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Constants.logger.i('Successfully connect to Firebase!');
   runApp(const MyApp());
 }
 
